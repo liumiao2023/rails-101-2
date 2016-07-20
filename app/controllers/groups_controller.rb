6c1332c43435b754016a45c04a2dd48d19970f00
liumiao2023 <liumiao2023@hotmail.com>
2016-07-20 13:15:11 +0800

    render partial with collection

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,7 @@
 class GroupsController < ApplicationController
 
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destory]
-  before_action :find_group_and_check_premission, only: [:edit, :updat, :destory]
+  before_action :find_group_and_check_premission, only: [:edit, :update, :destory]
 
   def index
     @groups = Group.all
@@ -33,14 +33,15 @@ class GroupsController < ApplicationController
   end
 
   def update
-    if @group.update(group_params)
-      redirect_to groups_path, notice: "Update Success"
-    else
-      render :edit
-    end
+  if @group.update(group_params)
+    redirect_to groups_path, notice: "Update Success"
+  else
+    render :edit
+  end
   end
 
   def destroy
+    @group = Group.find(params[:id])
     @group.destroy
     redirect_to groups_path, alert: "Group deleted"
   end
